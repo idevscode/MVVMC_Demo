a0a3a038e17a2f92cf58be5350d24f74bca0c4bc
idevscode <mail2dilshadali@gmail.com>
2025-08-22 07:17:45 +0530

    Initial Commit

---
+++ AssignmentFreelance/AssignmentFreelanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentFreelanceApp.swift
+//  AssignmentFreelance
+//
+//  Created by dilshad haidari on 22/08/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentFreelanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AssignmentFreelanceTests/AssignmentFreelanceTests.swift
@@ -0,0 +1,16 @@
+//
+//  AssignmentFreelanceTests.swift
+//  AssignmentFreelanceTests
+//
+//  Created by dilshad haidari on 22/08/25.
+//
+
+import Testing
+
+struct AssignmentFreelanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
